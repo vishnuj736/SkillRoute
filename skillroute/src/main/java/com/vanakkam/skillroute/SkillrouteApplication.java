97822e994297441f28b9fca45aa6e94027e8f2ce
vishnuj736 <vishnujeeva2006@gmail.com>
2026-05-16 19:22:07 +0530

    Feat: create backend

---
+++ skillroute/src/main/java/com/vanakkam/skillroute/SkillrouteApplication.java
@@ -0,0 +1,13 @@
+package com.vanakkam.skillroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillrouteApplication.class, args);
+	}
+
+}
